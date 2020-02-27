6190173a5b57a1bab5cc9c65ca431b0c4c66f525
Tomas Knapen <tknapen@gmail.com>
2020-02-27 15:17:20 +0100

    slight edits to fixation stim setup

--- hrfmapper.py
+++ hrfmapper.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 from psychopy.visual import GratingStim, Circle
 
+
 class HRFMapperSession(Session):
     def create_trials(self):
         """ Creates trials before running the session"""
@@ -11,32 +12,42 @@ class HRFMapperSession(Session):
         # get stimuli
         with h5py.File(exp_s['stim_file'], 'r') as f:
             self.stim_rgb_arr = np.array(f['patterns'])
-        self.stimuli = [GratingStim(win=self.session.win, tex=s.T) for s in self.stim_rgb_arr]
+        self.stimuli = [GratingStim(win=self.session.win, tex=s.T)
+                                    for s in self.stim_rgb_arr]
         self.fixation_dot = Circle(self.win, radius=0.1, edges=100, color='r')
 
         # timings that define how many trials etc.
         mean_trial_duration = exp_s['isi_min'] + exp_s['isi_mean']
-        self.n_trials = round((exp_s['total_run_duration'] - (exp_s['start_end_period'] * 2)) / mean_trial_duration)
-        tolerance_range = [exp_s['total_run_duration'] - exp_s['temporal_tolerance'], exp_s['total_run_duration'] + exp_s['temporal_tolerance']]
+        self.n_trials = round(
+            (exp_s['total_run_duration'] - (exp_s['start_end_period'] * 2)) / mean_trial_duration)
+        tolerance_range = [exp_s['total_run_duration'] - exp_s['temporal_tolerance'],
+            exp_s['total_run_duration'] + exp_s['temporal_tolerance']]
 
         # and then search for the right isis to fill up the experiment exactly.
-        isis = np.random.exponential(exp_s['isi_mean'], n_trials) + exp_s['isi_min']
+        isis = np.random.exponential(
+            exp_s['isi_mean'], n_trials) + exp_s['isi_min']
         exp_duration = isis + (exp_s['start_end_period'] * 2)
         while exp_duration < tolerance_range[0] or exp_duration > tolerance_range[1]:
-            isis = np.random.exponential(exp_s['isi_mean'], n_trials) + exp_s['isi_min']
+            isis = np.random.exponential(
+                exp_s['isi_mean'], n_trials) + exp_s['isi_min']
             exp_duration = isis + (exp_s['start_end_period'] * 2)
+        pretimes = np.zeros(self.n_trials)
+        pretimes[[0, -1]] = exp_s['start_end_period']
 
         # deciding which stimuli to show when
-        self.how_many_images_per_trial = exp_s['stim_flicker_freq'] * exp_s['stim_duration']
+        self.how_many_images_per_trial = exp_s['stim_flicker_freq'] * \
+            exp_s['stim_duration']
         self.duration_per_image = 1.0/exp_s['stim_flicker_freq']
 
-        trial_images = np.random.randint(len(self.stimuli), size=(self.n_trials, self.how_many_images_per_trial))
+        trial_images = np.random.randint(len(self.stimuli), size=(
+            self.n_trials, self.how_many_images_per_trial))
 
         for i in range(self.n_trials):
             self.trials.append(HRFMapperTrial(
                     session=self,
                     trial_nr=i,
-                    phase_durations=[0.0, exp_s['stim_duration'], isis[i]-exp_s['stim_duration']],
+                    phase_durations=[
+                        pretimes[i], exp_s['stim_duration'], isis[i]-exp_s['stim_duration']],
                     phase_names=['slack', 'stim', 'isi'],
                     parameters={'isi': isis[i],
                                 'stim_list': trial_images[i]},
@@ -101,7 +112,6 @@ class HRFMapperTrial(Trial):
             self.last_stim_time = self.session.clock.getTime()
             total_stim_time = self.last_stim_time - self.last_fix_time
             stim_index = min(total_stim_time // self.duration_per_image, len(self.parameters['stim_list']-1)
-            self.session.stimuli[self.parameters['stim_list'][stim_index]].draw()
-        self.fixation_dot.draw()
-
-
+            self.session.stimuli[self.parameters['stim_list']
+                [stim_index]].draw()
+        self.session.fixation_dot.draw()
